4cda533e3d3475522185e7ac28a6c64986a4c7eb
weihua.hwh <weihua.hwh@alibaba-inc.com>
2016-10-20 20:42:53 +0800

    Initial Commit

---
+++ YTUITestCases/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTUITestCases
+//
+//  Created by huanwh on 2016/10/20.
+//  Copyright © 2016年 hwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
